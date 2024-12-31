87c740f291a129f32e359213b7480a507f7411f2
phillip <douglasskidsfather@gmail.com>
2024-12-31 18:27:21 -0500

    top bar bug fixes

--- app/src/main/java/com/example/chatterplay/screens/MainScreen.kt
+++ app/src/main/java/com/example/chatterplay/screens/MainScreen.kt
@@ -3,6 +3,7 @@ package com.example.chatterplay.screens
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,8 +21,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowCircleDown
 import androidx.compose.material.icons.filled.HideImage
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ImageAspectRatio
 import androidx.compose.material.icons.filled.Man
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
@@ -34,8 +38,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -58,6 +64,8 @@ import com.example.chatterplay.seperate_composables.AllMembersRow
 import com.example.chatterplay.seperate_composables.ChatBubble
 import com.example.chatterplay.seperate_composables.ChatInput
 import com.example.chatterplay.seperate_composables.ChatLazyColumn
+import com.example.chatterplay.seperate_composables.ChatRiseTopBar
+import com.example.chatterplay.seperate_composables.NavigationRow
 import com.example.chatterplay.seperate_composables.rememberCRProfile
 import com.example.chatterplay.seperate_composables.rememberProfileState
 import com.example.chatterplay.ui.theme.CRAppTheme
@@ -72,10 +80,19 @@ fun MainScreen(CRRoomId: String, navController: NavController, viewModel: ChatVi
 
     val coroutineScope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
+    var showTopBarInfo by remember { mutableStateOf(false)}
 
 
     val profile = rememberCRProfile(CRRoomId = CRRoomId)
     val chatRoomMembers by viewModel.chatRoomMembers.collectAsState()
+
+    val tabs = listOf(
+        "null" to Icons.Default.Home,
+        "null" to Icons.Default.Person,
+        "null" to Icons.Default.Menu,
+        "null" to Icons.Default.ImageAspectRatio
+    )
+    var selectedTabindex by remember { mutableStateOf(1) }
 
     LaunchedEffect(CRRoomId){
         viewModel.fetchChatRoomMembers(roomId = CRRoomId, game = true)
@@ -93,7 +110,13 @@ fun MainScreen(CRRoomId: String, navController: NavController, viewModel: ChatVi
         content = {
             Scaffold(
                 topBar = {
-                    MainTopAppBar(
+                    ChatRiseTopBar(
+                        onClick = {showTopBarInfo = !showTopBarInfo},
+                        onAction = { coroutineScope.launch { drawerState.open() }},
+                        showTopBarInfo = showTopBarInfo,
+                        navController = navController
+                    )
+                    /*MainTopAppBar(
                         title = "ChatRise",
                         action = true,
                         actionIcon = Icons.Default.Menu,
@@ -101,7 +124,7 @@ fun MainScreen(CRRoomId: String, navController: NavController, viewModel: ChatVi
                             coroutineScope.launch { drawerState.open() }
                         },
                         navController = navController
-                        )
+                        )*/
                 },
                 bottomBar = {
                     ChatInput(
@@ -116,7 +139,15 @@ fun MainScreen(CRRoomId: String, navController: NavController, viewModel: ChatVi
                             .fillMaxSize()
                             .background(customPurple)
                             .padding(paddingValues)
+                            .clickable { showTopBarInfo = false }
                     ){
+                        NavigationRow(
+                            tabs = tabs,
+                            selectedTabIndex = selectedTabindex,
+                            onTabSelected = {index ->
+                                selectedTabindex = index
+                            }
+                        )
                         AllMembersRow(
                             chatRoomMembers = chatRoomMembers,
                             game = true,
