85d8c1962ce327cf053609b523f5a1022874723a
phillip <douglasskidsfather@gmail.com>
2025-01-13 12:46:24 -0500

    question 1 by 1

--- app/src/main/java/com/example/chatterplay/screens/GameScreen.kt
+++ app/src/main/java/com/example/chatterplay/screens/GameScreen.kt
@@ -1,21 +1,30 @@
 package com.example.chatterplay.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -67,6 +76,8 @@ answer selection of other players
 @Composable
 fun GameScreen(crViewModel: ChatRiseViewModel = viewModel()){
     val questions by crViewModel.gameQuestion.collectAsState()
+    val currentQuestionIndex = remember { mutableStateOf(0) }
+
     LaunchedEffect(true){
         crViewModel.fetchQuestions(2)
     }
@@ -76,7 +87,8 @@ fun GameScreen(crViewModel: ChatRiseViewModel = viewModel()){
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.LightGray)
+            .verticalScroll(rememberScrollState())
+            .background(CRAppTheme.colorScheme.onGameBackground)
     ){
         if(questions.isEmpty()){
             Text(
@@ -85,16 +97,76 @@ fun GameScreen(crViewModel: ChatRiseViewModel = viewModel()){
                 style = CRAppTheme.typography.H1
             )
         } else {
-            questions.forEachIndexed() { index, question ->
+            if (currentQuestionIndex.value < questions.size){
+                PairQuestion(
+                    questions[currentQuestionIndex.value].Question,
+                    onAnswer = {
+                        currentQuestionIndex.value += 1 // next question
+                    }
+                )
+            } else {
                 Text(
-                    question.Question,
+                    "Done!",
                     color = Color.Black,
-                    fontSize = 15.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(bottom = 30.dp)
+                    style = CRAppTheme.typography.H1,
+                    modifier = Modifier.padding(20.dp)
                 )
             }
+        }
+    }
+}
+
+
+@Composable
+fun PairQuestion(
+    question: String,
+    onAnswer: () -> Unit
+){
+    Column (
+    ) {
+        Text(
+            question,
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp) // outside
+                .padding(top = 20.dp)
+                .background(CRAppTheme.colorScheme.gameBackground)
+                .padding(15.dp) // inside
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp)
+        ){
+            Text(
+                "Yes",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                modifier = Modifier
+                    .width(100.dp)
+                    .padding(10.dp) // outside
+                    .background(CRAppTheme.colorScheme.gameBackground)
+                    .padding(15.dp) // inside
+                    .clickable {
+                        onAnswer()
+                    }
+            )
+            Text(
+                "No",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                modifier = Modifier
+                    .width(100.dp)
+                    .padding(10.dp) // outside
+                    .background(CRAppTheme.colorScheme.gameBackground)
+                    .padding(15.dp) // inside
+                    .clickable {
+                        onAnswer()
+                    }
+            )
         }
     }
 }
